4204878fc93811685477e93173eb90387a4a1fca
Marionetko <webius.net@gmail.com>
2023-05-25 12:27:18 +0300

    folder structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import About from '../views/AboutView.vue'
+import Jobs from '../views/jobs/Jobs.vue'
 
 const routes = [
   {
@@ -12,7 +13,12 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About
-  }
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: Jobs
+  },
 ]
 
 const router = createRouter({
